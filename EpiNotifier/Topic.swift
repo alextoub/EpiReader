c4f64507f535b9493c6da9c6d9c859e358feb386
Alexandre Toubiana <toubia_b@epita.fr>
2017-03-20 13:17:56 +0100

    [MODEL] Create class for topic

---
+++ EpiNotifier/Topic.swift
@@ -0,0 +1,38 @@
+//
+//  Topic.swift
+//  EpiNotifier
+//
+//  Created by Alexandre Toubiana on 20/03/2017.
+//  Copyright © 2017 Alexandre Toubiana. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class Topic: Mappable {
+  
+  // MARK: - Attributes
+  var id: Int?
+  var uid: String?
+  var author: String?
+  var subject: String?
+  var content: String?
+  var creation_date: String?
+  var groups: [String]?
+  var children: [Topic]?
+
+  // MARK: - ObjectMapper functions
+  required init?(map: Map) {
+  }
+  
+  func mapping(map: Map) {
+    self.id            <- map["id"]
+    self.uid           <- map["uid"]
+    self.author        <- map["author"]
+    self.subject       <- map["subject"]
+    self.content       <- map["content"]
+    self.creation_date <- map["creation_date"]
+    self.groups        <- map["groups"]
+    self.children      <- map["children"]
+  }
+}
