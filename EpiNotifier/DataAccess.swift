20a6c4ca9da352a9868618b63137a66d193e3dd2
Alexandre Toubiana <toubia_b@epita.fr>
2017-03-20 14:20:34 +0100

    [SANITY] Add .gitignore

---
+++ EpiNotifier/DataAccess.swift
@@ -0,0 +1,9 @@
+//
+//  DataAccess.swift
+//  EpiNotifier
+//
+//  Created by Alexandre Toubiana on 20/03/2017.
+//  Copyright © 2017 Alexandre Toubiana. All rights reserved.
+//
+
+import Foundation
---
+++ EpiNotifier/DataBusiness.swift
@@ -0,0 +1,9 @@
+//
+//  DataBusiness.swift
+//  EpiNotifier
+//
+//  Created by Alexandre Toubiana on 20/03/2017.
+//  Copyright © 2017 Alexandre Toubiana. All rights reserved.
+//
+
+import Foundation
