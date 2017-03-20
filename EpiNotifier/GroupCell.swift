b0136afa74729529087b9bb71ab59e7064bf5a3d
Alexandre Toubiana <toubia_b@epita.fr>
2017-03-20 13:16:59 +0100

    [GROUP] Add outlets to cell

--- EpiNotifier/GroupCell.swift
+++ EpiNotifier/GroupCell.swift
@@ -9,16 +9,17 @@
 import UIKit
 
 class GroupCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+  
+  @IBOutlet weak var groupNameLabel: UILabel!
+  @IBOutlet weak var groupView: UIView!
+  @IBOutlet weak var nbNewsLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+  
 }
