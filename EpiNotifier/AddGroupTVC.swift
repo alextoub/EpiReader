01ba39337a632727da88acddd4c86ca3c63bdae9
Alexandre Toubiana <toubia_b@epita.fr>
2017-03-20 14:38:42 +0100

    [GROUP] Create view to add group to favorite

---
+++ EpiNotifier/AddGroupTVC.swift
@@ -0,0 +1,107 @@
+//
+//  AddGroupTVC.swift
+//  EpiNotifier
+//
+//  Created by Alexandre Toubiana on 20/03/2017.
+//  Copyright © 2017 Alexandre Toubiana. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+class AddGroupTVC: UITableViewController {
+  
+  var groups = [Group]()
+  var favorites = [Favorite]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupGroup()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  func setupGroup() {
+    getGroups()
+    SVProgressHUD.setDefaultMaskType(.black)
+    SVProgressHUD.show(withStatus: "Chargement en cours")
+  }
+  
+  func getGroups(){
+    MainBusiness.getGroups { (response, error) in
+      DispatchQueue.main.async {
+        if error == nil {
+          self.groups = response!
+          self.tableView.reloadData()
+          SVProgressHUD.dismiss()
+        }
+      }
+    }
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return groups.count
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "AddGroupCell", for: indexPath) as! AddGroupCell
+    let index = groups[indexPath.row]
+    cell.groupNameLabel.text = index.group_name
+    return cell
+  }
+  
+  /*
+   // Override to support conditional editing of the table view.
+   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the specified item to be editable.
+   return true
+   }
+   */
+  
+  /*
+   // Override to support editing the table view.
+   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+   if editingStyle == .delete {
+   // Delete the row from the data source
+   tableView.deleteRows(at: [indexPath], with: .fade)
+   } else if editingStyle == .insert {
+   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+   }
+   }
+   */
+  
+  /*
+   // Override to support rearranging the table view.
+   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+   
+   }
+   */
+  
+  /*
+   // Override to support conditional rearranging of the table view.
+   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the item to be re-orderable.
+   return true
+   }
+   */
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
