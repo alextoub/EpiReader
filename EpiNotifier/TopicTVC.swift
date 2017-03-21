16e781d295c075ee0d1b14e8ad97b946b0f21a63
Alexandre Toubiana <toubia_b@epita.fr>
2017-03-21 01:31:29 +0100

    [TOPIC] Increase size of cell for height of textView

--- EpiNotifier/TopicTVC.swift
+++ EpiNotifier/TopicTVC.swift
@@ -16,10 +16,19 @@ class TopicTVC: UITableViewController {
   var topic: Topic?
   var current: Topic?
   var index = 0
+  var sizeCells = [CGFloat]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     setupTopic()
+  }
+  
+  func setupSizeCells() {
+    var i = 0
+    while i < nb_msg {
+      sizeCells.append(195.0)
+      i += 1
+    }
   }
   
   override func didReceiveMemoryWarning() {
@@ -39,6 +48,7 @@ class TopicTVC: UITableViewController {
           self.topic = response
           self.current = self.topic
           self.index = 0
+          self.setupSizeCells()
           self.tableView.reloadData()
           SVProgressHUD.dismiss()
         }
@@ -58,18 +68,29 @@ class TopicTVC: UITableViewController {
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "TopicCell", for: indexPath) as! TopicCell
-    cell.authorLabel.text = current?.author
+    guard (current != nil) else {
+      return cell
+    }
+    print(cell.bounds.height)
+    cell.authorLabel.text = parseAuthor((current?.author)!)[0]
     cell.contentText.text = current?.content
     cell.subjectLabel.text = current?.subject
-    cell.dateLabel.text = current?.creation_date
-    let size = UITableViewAutomaticDimension
-    cell.newsView.frame = CGRect(x: cell.newsView.bounds.minX, y: cell.newsView.bounds.minY, width: cell.newsView.bounds.width, height: size - 10)
+    cell.dateLabel.text = StrToAbrevWithHour(dateStr: (current?.creation_date)!)
+    sizeCells[indexPath.row] = cell.contentText.contentSize.height
+    cell.contentText.sizeThatFits(CGSize(width: cell.contentText.contentSize.width, height: cell.contentText.contentSize.height))
     //cell.newsView.bounds.height = UITableViewAutomaticDimension - 10
     if nb_msg > 1 && index < nb_msg - 1 {
       current = current?.children?[index]
       index += 1
     }
     return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    guard (sizeCells.count > 0) else {
+      return 195
+    }
+    return 195 - 141 + sizeCells[indexPath.row]
   }
   
 }
