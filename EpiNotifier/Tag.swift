6a8a466364b15516d964b4fbc2dd3ff72bbee4cf
Alexandre Toubiana <toubia_b@epita.fr>
2017-03-23 12:56:02 +0100

    [TAG] Create class

---
+++ EpiNotifier/Tag.swift
@@ -0,0 +1,50 @@
+//
+//  Tag.swift
+//  EpiNotifier
+//
+//  Created by Alexandre Toubiana on 23/03/2017.
+//  Copyright © 2017 Alexandre Toubiana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Tag: NSObject, NSCoding {
+  
+  //MARK: Properties
+  
+  var tagName: String?
+  var attributedColor: UIColor?
+  
+  //MARK: Archiving Paths
+  
+  static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+  static let ArchiveURL = DocumentsDirectory.appendingPathComponent("tag")
+  
+  //MARK: Types
+  
+  struct PropertyKey {
+    static let tagName = "CtagName"
+    static let attributedColor = "CattributedColor"
+  }
+  
+  //MARK: Initialization
+  
+  init(tagName: String, attributedColor: UIColor) {
+    self.tagName = tagName
+    self.attributedColor = attributedColor
+  }
+  
+  //MARK: NSCoding
+  
+  func encode(with aCoder: NSCoder) {
+    aCoder.encode(tagName, forKey: PropertyKey.tagName)
+    aCoder.encode(attributedColor, forKey: PropertyKey.attributedColor)
+  }
+  
+  required convenience init?(coder aDecoder: NSCoder) {
+    let tagName = aDecoder.decodeObject(forKey: PropertyKey.tagName) as? String
+    let attributedColor = aDecoder.decodeObject(forKey: PropertyKey.attributedColor) as? UIColor
+    self.init(tagName: tagName!, attributedColor: attributedColor!)
+  }
+}
