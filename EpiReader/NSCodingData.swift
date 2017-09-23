ede7a26597cb953fa396aed0efe0cc014f312d52
Alexandre Toubiana <toubia_b@epita.fr>
2017-09-23 12:40:17 -0700

    Add icon notifs / create file NSCodingData / Add tableview to settings

---
+++ EpiReader/NSCodingData.swift
@@ -0,0 +1,51 @@
+//
+//  NSCodingData.swift
+//  EpiReader
+//
+//  Created by Alexandre Toubiana on 23/09/2017.
+//  Copyright © 2017 Alexandre Toubiana. All rights reserved.
+//
+
+import Foundation
+
+class NSCodingData {
+    public func saveFavorites(favorites: [Favorite]) {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(favorites, toFile: Favorite.ArchiveURL.path)
+        if isSuccessfulSave {
+            print("Favorites successfully saved.")
+        } else {
+            print("Failed to save favorites...")
+        }
+    }
+    
+    public func loadFavorites() -> [Favorite]?  {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Favorite.ArchiveURL.path) as? [Favorite]
+    }
+    
+    public func saveReadNews(readNews: [ReadNews]) {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(readNews, toFile: ReadNews.ArchiveURL.path)
+        if isSuccessfulSave {
+            print("ReadNews successfully saved.")
+        } else {
+            print("Failed to save readNews...")
+        }
+    }
+    
+    public func loadReadNews() -> [ReadNews]?  {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: ReadNews.ArchiveURL.path) as? [ReadNews]
+    }
+    
+    public func saveTag(tags: [Tag]) {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(tags, toFile: Tag.ArchiveURL.path)
+        if isSuccessfulSave {
+            print("Tag successfully saved.")
+        } else {
+            print("Failed to save tag...")
+        }
+    }
+    
+    public func loadTag() -> [Tag]?  {
+        print(Tag.ArchiveURL.path)
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Tag.ArchiveURL.path) as? [Tag]
+    }
+}
